ed722e16f86ba96a5de25e4e17c79957d57d725b
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-01-08 16:22:48 +0900

    set to GKMonteCarloStrategist

--- reversi/reversi/MinmaxReversi.swift
+++ reversi/reversi/MinmaxReversi.swift
@@ -130,12 +130,13 @@ class ReversiModel: NSObject, GKGameModel {
 }
     
 struct MinmaxReversi: ReversiStrategy {
-    let strategist = GKMinmaxStrategist()
+//    let strategist = GKMinmaxStrategist()
+    let strategist = GKMonteCarloStrategist()
     let gameModel = ReversiModel()
     init() {
         strategist.gameModel = gameModel
-//        strategist.explorationParameter = 1
-        strategist.maxLookAheadDepth = 2
+        strategist.explorationParameter = 1
+//        strategist.maxLookAheadDepth = 2
     }
     func predict(_ board: [State],  _ targetPlayer: State, completion: ([Float32]) -> Void) {
         gameModel.updateState(board, targetPlayer)
